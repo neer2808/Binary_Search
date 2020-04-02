38eef657abe21426ce39643ccc0016e4b68c2d61
neer2808 <uic.neerajkhanna@gmail.com>
2020-04-02 16:13:15 +0530

    defining the logic for inner and outer loop

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -10,21 +10,21 @@
 //after completing all the above steps we will get the sorted array
 
 
-
-
-
-
 public class InsertionSort {
   public static void main(String[] args) {
     int val ;
-
     int j ;
     int arr[] = {30, 10, 50, 20, 90, 40};
-    for(int i =0;i<arr.length;i++)
-    {
+
+
+    // outer loop that keeps track of how many times the insertion needs to be done
+
+    // here i am initializing the variable i with 1 because  we can insert first value directly into the sorted side of the array directly
+    for(int i =1;i<arr.length;i++)  // from  1st index it needs to compare the values with the values already available on the sorted side
+    {                               // of the array
       val = arr[i];
       j = i-1;
-
+ // inner loop that takes care of swapping and comparision tasks
         while(j>=0 && arr[j]>val)
         {
           arr[j+1]= arr[j];
